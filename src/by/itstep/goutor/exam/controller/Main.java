7828f9aedcc4a451c7ae7ef75caeed5e44371de8
nikishkaa <nikita.goutor@gmail.com>
2024-05-02 19:18:55 +0300

    Initial commit

---
+++ src/by/itstep/goutor/exam/controller/Main.java
@@ -0,0 +1,7 @@
+package by.itstep.goutor.exam.controller;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/itstep/goutor/exam/view/Print.java
@@ -0,0 +1,7 @@
+package by.itstep.goutor.exam.view;
+
+public class Print {
+    public static void printer(String msg) {
+        System.out.print(msg);
+    }
+}
